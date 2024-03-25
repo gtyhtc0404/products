36f7c71fc31771823dfd883c4e178324c07e021a
Patti <pattishhuang@gmail.com>
2024-03-25 15:52:20 +0800

    add read file and header

--- products.py
+++ products.py
@@ -1,13 +1,31 @@
+# 讀取檔案
 products = []
-while True:
-	name = input('請輸入商品名稱：')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格：')
-	products.append([name, price])
+with open ('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品，價格' in line:
+            continue  # 繼續 直接跳到下一回(還在迴圈中) 通常寫在迴圈中很前面的位置
+        name, price = line.strip().split(',')
+        products.append([name, price])
 print(products)
-print(products[0])
-print(products[0][0])
 
+# 輸入新資料
+while True:
+    name = input('請輸入商品名稱：')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格：')
+    products.append([name, price])
+
+# 印出所有商品紀錄
 for p in products:
-	print(p[0], '的價格是：', p[1]) 
+    if p[0] == '商品':
+        continue
+    print(p[0], '的價格是：', p[1]) 
+    
+# 寫入資料(追加新資料)
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        if p[0] == '商品':
+            continue
+        f.write(p[0] + ',' + p[1] + '\n')
